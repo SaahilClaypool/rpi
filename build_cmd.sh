c80bca439515003e5168e2ed61b272938943cd2c
saahilclaypool <saahil@claypools.org>
2018-10-18 18:40:20 -0400

    fixes build script

--- build_cmd.sh
+++ build_cmd.sh
@@ -1,19 +1,23 @@
 cd /linux
+echo $_UID
+echo $_GID
+
 KERNEL=kernel7
+make mrproper
 make O=/out ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig -j8
 make O=/out ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs -j8
-mkdir /mnt
-mkdir /mnt/fat32
-mkdir /mnt/ext4
+mkdir /build
+mkdir /build/fat32
+mkdir /build/ext4
 
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=/mnt/ext4 modules_install -j8
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- INSTALL_MOD_PATH=/build/ext4 modules_install -j8
 
-# cp /mnt/fat32/$KERNEL.img /mnt/fat32/$KERNEL-backup.img
-cp arch/arm/boot/zImage /mnt/fat32/$KERNEL.img
-cp arch/arm/boot/dts/*.dtb /mnt/fat32/
-cp arch/arm/boot/dts/overlays/*.dtb* /mnt/fat32/overlays/
-cp arch/arm/boot/dts/overlays/README /mnt/fat32/overlays/
+# cp /build/fat32/$KERNEL.img /build/fat32/$KERNEL-backup.img
+cp arch/arm/boot/zImage /build/fat32/$KERNEL.img
+cp arch/arm/boot/dts/*.dtb /build/fat32/
+cp arch/arm/boot/dts/overlays/*.dtb* /build/fat32/overlays/
+cp arch/arm/boot/dts/overlays/README /build/fat32/overlays/
 
-mv /mnt $OUT
+mv /build $OUT
 
-chown -R $OUT $_UID $_GID
+chown -R $_UID:$_GID $OUT 
