8bcf5cd82e72480a66d99aecf390ebcf8d161b30
saahilclaypool <saahil@claypools.org>
2018-11-13 15:48:28 -0500

    Ran tests on three network emulators

--- Config/Validation/temp.sh
+++ Config/Validation/temp.sh
@@ -1,4 +1,3 @@
-./start_trial.py ./80mbps_one.json;
-./start_trial.py ./80mbps_peak_one.json;
-./start_trial.py ./80mbps_netem_one.json;
-./start_trial.py ./80mbps_cake_one.json;
+./start_trial.py ./Configs/80m_bbr_netem.json;
+./start_trial.py ./Configs/80m_bbr_tbf.json;
+./start_trial.py ./Configs/80m_bbr_tbf_peak.json;
