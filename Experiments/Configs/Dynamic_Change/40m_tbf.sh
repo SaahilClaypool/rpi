0155a7718f465f2cb53857c444c0f7c1d1a14d44
saahilclaypool <saahil@claypools.org>
2018-12-02 17:59:23 -0500

    Good bbr results

---
+++ Experiments/Configs/Dynamic_Change/40m_tbf.sh
@@ -0,0 +1,10 @@
+sudo tc qdisc del dev enp3s0 root
+
+sudo tc qdisc add dev enp3s0 root handle 1:0 netem delay 10ms limit 1000
+sudo tc qdisc add dev enp3s0 parent 1:1 handle 10: tbf rate 40mbit buffer 1mbit limit 1000mbit 
+sudo tc qdisc add dev enp3s0 parent 10:1 handle 100: tbf rate 40mbit burst .05mbit limit 1000mbit 
+
+tc -s qdisc ls dev enp3s0
+
+# https://netbeez.net/blog/how-to-use-the-linux-traffic-control/
+# https://wiki.linuxfoundation.org/networking/netem
